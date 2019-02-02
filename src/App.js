a525c86ad448871fab0f3cd44dc68493157af78f
Jason_Shu <shuyue524@gmail.com>
2019-02-02 19:22:18 +0800

    my first react demo

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+  add() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <span className="count-box">{this.state.count}</span>
+        <button onClick={this.add.bind(this)}>click me!</button>
       </div>
     );
   }
