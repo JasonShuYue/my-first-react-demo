6d332790fe48a88c0d1c1c76748f5513061791ff
Jason_Shu <shuyue524@gmail.com>
2019-02-02 19:28:06 +0800

    npm run build

---
+++ build/precache-manifest.d7003c88873adc531782442ff6a10f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-react-demo/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "659aaa7407aa0935d004",
+    "url": "/my-first-react-demo/build/static/js/runtime~main.659aaa74.js"
+  },
+  {
+    "revision": "5e63fb80613f5c4a305e",
+    "url": "/my-first-react-demo/build/static/js/main.5e63fb80.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/my-first-react-demo/build/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "5e63fb80613f5c4a305e",
+    "url": "/my-first-react-demo/build/static/css/main.019ea72a.chunk.css"
+  },
+  {
+    "revision": "d2f79619e13d69a7a8597b7b5b2aedc7",
+    "url": "/my-first-react-demo/build/index.html"
+  }
+];
